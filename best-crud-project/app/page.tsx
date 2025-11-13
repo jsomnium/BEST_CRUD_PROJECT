2097f9429e36c2350b5df26efab716c1ef49389d
jsomnium <jsomnium@naver.com>
2025-11-13 14:21:19 +0900

    init: Initial project setup

---
+++ best-crud-project/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <main className="text-amber-900">BEST_CRUD_PROJECT</main>
+    </div>
+  );
+}
