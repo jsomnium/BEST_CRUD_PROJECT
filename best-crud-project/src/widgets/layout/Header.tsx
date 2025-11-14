bc108dc986ccf38fc9e4587972d180c66c8f6570
jsomnium <jsomnium@naver.com>
2025-11-14 16:11:29 +0900

    feat(header): check user state and render signin button

--- best-crud-project/src/widgets/layout/Header.tsx
+++ best-crud-project/src/widgets/layout/Header.tsx
@@ -1,13 +1,35 @@
+'use client';
+
 import Link from 'next/link';
 import ROUTES from '@/src/shared/constants/routes';
+import { useAuthStore } from '@/src/entities/user/userStore';
 
 export default function Header() {
+  const { isLoggedIn, user, logout } = useAuthStore();
+
+  const handleLogout = () => {
+    console.log('로그아웃 처리');
+    logout();
+  };
+
   return (
     <header className="flex justify-between px-8 py-4">
       <Link href={ROUTES.HOME} className="text-primary flex text-2xl font-bold">
         [Logo]
       </Link>
-      <Link href={ROUTES.SIGN_IN}>로그인</Link>
+      {isLoggedIn ? (
+        <div className="space-x-6">
+          <span className="text-grey-750">
+            안녕하세요,
+            <span className="text-primary font-medium">{user?.nickname}</span>님
+          </span>
+          <button onClick={handleLogout} className="cursor-pointer">
+            로그아웃
+          </button>
+        </div>
+      ) : (
+        <Link href={ROUTES.SIGN_IN}>로그인</Link>
+      )}
     </header>
   );
 }
