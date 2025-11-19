ddd229aa7c840783c5048b1bbcb06558b6b4bd67
jsomnium <jsomnium@naver.com>
2025-11-19 19:06:16 +0900

    fix: fix import

--- best-crud-project/src/widgets/ui/PostList.tsx
+++ best-crud-project/src/widgets/ui/PostList.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { PostItem } from '@/src/entities/post/PostItem';
-import { Post } from '@/src/entities/post/types/post-type';
+import { PostItem } from '../../entities/post/PostItem';
+import { Post } from '../../entities/post/types/post-type';
 import { usePostSelectionStore } from '@/src/features/posts/model/usePostSelectionStore';
 
 interface PostListHeaderProps {
